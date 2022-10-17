c5f20d92f1a8d10fbb78b7538c55f8ce27e8fd53
Matteo Russo <Matteo.Russo@docflow.it>
2022-10-17 09:18:07 +0200

    first commit

---
+++ Assets/Scripts/Button_Script.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Button_Script : MonoBehaviour
+{
+    public void Start_Game()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
