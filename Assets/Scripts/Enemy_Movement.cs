ccdf5a01d97af4b5876d9ee4f837d6a3fedeeacd
Matteo Russo <Matteo.Russo@docflow.it>
2022-10-22 22:38:35 +0200

    test

---
+++ Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    [SerializeField] private float speed = 2.5f;
+    private BoxCollider2D boxCollider;
+    private SpriteRenderer sprite;
+    private int currentWaypointIndex = 0;
+
+    Coroutine Movement_Coroutine;
+
+    IEnumerator Movement()
+    {
+        while (true)
+        {
+            if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+            {
+                currentWaypointIndex++;
+                if (currentWaypointIndex >= waypoints.Length)
+                {
+                    currentWaypointIndex = 0;
+                }
+                if (CompareTag("Opossum"))
+                {
+                    sprite.flipX = !sprite.flipX;
+                }
+            }
+            transform.position = Vector2.MoveTowards
+                                (transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+            yield return null;
+        }
+    }
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        Movement_Coroutine = StartCoroutine(Movement());
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if(collision.collider.bounds.center.y > boxCollider.bounds.center.y + boxCollider.bounds.extents.y)
+            {
+                //Apply Force upwards to Player
+                collision.gameObject.GetComponent<Player_Movement>().applyForce = true;
+
+                //Trigger Death
+                Die();
+            }
+        }
+    }
+
+    private void Die()
+    {
+        //Change into Death animation and destroy
+        GetComponent<Animator>().SetBool("isDead", true);
+        boxCollider.enabled = false;
+        StopCoroutine(Movement_Coroutine);
+        Destroy(gameObject, 2f);
+    }
+}
---
+++ Assets/Scripts/Opossum_Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eagle_Movement : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    [SerializeField] private float speed = 2.5f;
+    private BoxCollider2D boxCollider;
+    private int currentWaypointIndex = 0;
+
+
+    IEnumerator Movement()
+    {
+        while (true)
+        {
+            if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+            {
+                currentWaypointIndex++;
+                if (currentWaypointIndex >= waypoints.Length)
+                {
+                    currentWaypointIndex = 0;
+                }
+            }
+            transform.position = Vector2.MoveTowards
+                                (transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+            yield return null;
+        }
+    }
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        StartCoroutine(Movement());
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if(collision.collider.bounds.center.y > boxCollider.bounds.center.y + boxCollider.bounds.extents.y)
+            {
+                collision.gameObject.GetComponent<Player_Movement>().applyForce = true;
+
+                //Trigger Death
+                GetComponent<Animator>().SetBool("isDead", true);
+                boxCollider.enabled = false;
+                Destroy(gameObject, 2f);
+            }
+        }
+    }
+}
