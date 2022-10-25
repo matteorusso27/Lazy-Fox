bea622ddf9d792724bc491daa4bb9ead0c7e2f04
Matteo Russo <Matteo.Russo@docflow.it>
2022-10-26 01:26:47 +0200

    Improved_UI

---
+++ Assets/Scripts/Listener_Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Listener_Script: MonoBehaviour
+{
+    [SerializeField] bool isStart;
+    private bool alreadyPressed;
+    [SerializeField] Text text;
+    private void Update()
+    {
+        if (!alreadyPressed)
+        {
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Return)) 
+            {
+                alreadyPressed = true;
+                if (isStart)
+                {
+                    Invoke("Start_Game", 1f);
+                    text.text = "Let's go";
+                }
+                else
+                {
+                    Application.Quit();
+                }
+            }
+        }
+    }
+    public void Start_Game()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/UITextBlinking.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextBlinking : MonoBehaviour
+{
+    private Coroutine BlinkTextCoroutine;
+    private Text text;
+    private float timeToWait;
+    private IEnumerator BlinkText()
+    {
+        while (true)
+        {
+           
+            if(text.color == Color.clear)
+            {
+                text.color = Color.black;
+                timeToWait = 1.2f;
+            }
+            else
+            {
+                text.color = Color.clear;
+                timeToWait = 0.3f;
+            }
+            yield return new WaitForSeconds(timeToWait);
+        }
+    }
+
+    void Start()
+    {
+        Debug.Log("partito");
+        text = GetComponent<Text>();
+        BlinkTextCoroutine = StartCoroutine(BlinkText());
+    }
+}
